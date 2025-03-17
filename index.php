ffe81cda20930cc73e57ac76deebb5c03311e327
Vitor Murgi Beloto <vitor.beloto@dinfor.uniceub>
2025-03-17 09:00:13 -0300

    arquivos iniciais

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div >
+        <form method="post" action="resultado.php">
+        N1:<input type="number" name="N1">
+        <br><br>
+        N2:<input type="number" name="N2"> 
+        <br><br>
+
+
+        <input type="submit" value="Somar">
+        <input type="reset" value="Limpar">
+</form>
+    </div>
+</body>
+</html>
