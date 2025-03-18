ff6675790d7e89d6881a866c973557883534035f
Vitor Murgi Beloto <vitor.beloto@dinfor.uniceub>
2025-03-18 08:31:47 -0300

    Implementadas as quatro operações básicas

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <div >
-        <form method="post" action="resultado.php">
+        <form method="post" action="resultadosoma.php">
         N1:<input type="number" name="N1">
         <br><br>
         N2:<input type="number" name="N2"> 
@@ -16,6 +16,45 @@
 
 
         <input type="submit" value="Somar">
+        <input type="reset" value="Limpar">
+</form>
+    </div>
+    <br><br>
+    <div >
+        <form method="post" action="resultadosub.php">
+        N1:<input type="number" name="N1">
+        <br><br>
+        N2:<input type="number" name="N2"> 
+        <br><br>
+
+
+        <input type="submit" value="Subtrair">
+        <input type="reset" value="Limpar">
+</form>
+    </div>
+    <br><br>
+    <div >
+        <form method="post" action="resultadomult.php">
+        N1:<input type="number" name="N1">
+        <br><br>
+        N2:<input type="number" name="N2"> 
+        <br><br>
+
+
+        <input type="submit" value="Multiplicar">
+        <input type="reset" value="Limpar">
+</form>
+    </div>
+    <br><br>
+    <div >
+        <form method="post" action="resultadodiv.php">
+        N1:<input type="number" name="N1">
+        <br><br>
+        N2:<input type="number" name="N2"> 
+        <br><br>
+
+
+        <input type="submit" value="Dividir">
         <input type="reset" value="Limpar">
 </form>
     </div>
---
+++ resultadodiv.php
@@ -0,0 +1,9 @@
+    <?php
+    $n1 = $_REQUEST['N1'];
+    $n2 = $_REQUEST['N2'];
+
+    echo $n1 / $n2;
+
+
+
+    ?> 
